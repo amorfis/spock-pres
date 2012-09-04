11ea9cf197dc2d76ff6d0cc977e86af41fd52fae
Pawel Stawicki <pawelstawicki@gmail.com>
2012-09-04 22:46:05 +0200

    Parametrization added.

---
+++ code/src/test/groovy/pl/szjug/spock/Test6Parametrized.groovy
@@ -0,0 +1,32 @@
+package pl.szjug.spock
+
+import org.junit.Test
+import spock.lang.Specification
+import spock.lang.Unroll
+
+/**
+ * @author Pawel Stawicki 
+   @since 7/15/12 1:03 PM
+ */
+class Test6Parametrized extends Specification {
+
+  //New for every test
+  def service = new NumbersService()
+
+  @Test
+  @Unroll
+  def 'isEven should return #isEven for #number'() {
+    when:
+    def result = service.isEven(number)
+
+    then:
+    result == isEven
+
+    where:
+    number  | isEven
+    2       | true
+    3       | false
+    65      | false
+  }
+
+}
